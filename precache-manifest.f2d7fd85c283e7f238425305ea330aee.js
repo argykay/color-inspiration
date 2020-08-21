71d877e387b24ae224e763217ca08133953d72d6
argykay <argykonstantina@gmail.com>
2020-08-21 02:47:08 +0200

    Updates

---
+++ precache-manifest.f2d7fd85c283e7f238425305ea330aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0369bda62e0b7cb3ab0ad8a37c708e6c",
+    "url": "/color-inspiration/index.html"
+  },
+  {
+    "revision": "86a1b46e0e5eafba7205",
+    "url": "/color-inspiration/static/css/main.777797d8.chunk.css"
+  },
+  {
+    "revision": "50162550577873d2ea8c",
+    "url": "/color-inspiration/static/js/2.ba7096ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-inspiration/static/js/2.ba7096ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a1b46e0e5eafba7205",
+    "url": "/color-inspiration/static/js/main.78777c41.chunk.js"
+  },
+  {
+    "revision": "96fa08905a53dac9660c",
+    "url": "/color-inspiration/static/js/runtime-main.375121d0.js"
+  }
+]);
